4cc2e25fd47616b8b61a98a0df36641da6becd15
Kaanaaaaachan <94956335+InigoMiguelArada@users.noreply.github.com>
2022-11-18 17:49:47 +0800

    Update user_account.js

--- models/user_account.js
+++ models/user_account.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
 const Users_accountSchema = new mongoose.Schema({
+  userID: {
+    type: Number,
+    required: true
+  },
   username: {
     type: String,
     required: true
