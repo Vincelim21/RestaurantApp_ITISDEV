000efe9c3e45349835fb1e83fd1c42169087841a
RaphaelDLSU <91138973+RaphaelDLSU@users.noreply.github.com>
2022-12-10 14:44:57 +0800

    Update cashiermenuscript.js

--- public/js/cashiermenuscript.js
+++ public/js/cashiermenuscript.js
@@ -1,12 +1,11 @@
 function addItem(id,orders){
-    
+
     $(".table-header").after('<tr class="table-input">\
     <td>'+id+'</td>\
     <td><input type="text" name="quantity_value" value=""></td>\
     <td>PRICE</td>\
     </tr>')
-    console.log("HERE ORDERS" + orders);
-
+   
     
 
 }
