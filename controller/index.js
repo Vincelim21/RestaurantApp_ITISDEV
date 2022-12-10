db131cbc743f80943e8764249ea233ad244532e1
Kaanaaaaachan <94956335+InigoMiguelArada@users.noreply.github.com>
2022-12-11 04:20:10 +0800

    remove extra const

--- controller/index.js
+++ controller/index.js
@@ -1,21 +1,7 @@
 const express = require('express')
-const IngredientOrderModel = require('../models/ingredient_order')//ingredient_order table
-const manualCountModel = require('../models/manual_count')
-const discrepancieModel = require('../models/discrepancie')
-const spoilageModel = require('../models/spoilage')
-const UserDetailsModel = require('../models/user_details')
-const CustomerOrderModel = require('../models/customer_order')
-const recipeModel = require('../models/recipe')
-const ingredientsModel = require('../models/ingredients')
-const recipeIngredientsModel = require('../models/recipe_ingredients')
-const IngredientsModel = require("../models/ingredients")
-const IngredientFirstModel = require("../models/ingredient_first")
-const IngredientStockModel = require("../models/ingredient_stock")
-const unitModel = require("../models/unit")
 const router = express.Router()
+const UserDetailsModel = require('../models/user_details')
 const mongoose = require('mongoose')
-const { addListener } = require('../models/ingredient_stock')
-const customerOrderModel = require('../models/customer_order')
 const db = mongoose.connection
 
 router.get('/', async(req,res) =>{
