fcd2a0f50de48c6a60bce76bc38559ef01e889a4
Kaanaaaaachan <94956335+InigoMiguelArada@users.noreply.github.com>
2022-12-11 15:42:35 +0800

    Added encrypt function but not called yet

--- models/user_details.js
+++ models/user_details.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const User_detailsSchema = new mongoose.Schema({
     userID: {
@@ -13,7 +14,7 @@ const User_detailsSchema = new mongoose.Schema({
         type: String,
         required : true
     },
-    username: {
+    email: {
         type: String,
         required : true
     },
@@ -26,5 +27,28 @@ const User_detailsSchema = new mongoose.Schema({
       required : true
     }
 }, {collection : 'user_details'})
+
+// still figuring out how to call this one to encrypt
+User_detailsSchema.pre('save', async function (next) {
+  try{
+    // console.log("Called before saving a user")
+    const salt = await bcrypt.genSalt(10)
+    console.log(this.email, this.password)
+    const hashedPassword = await bcrypt.hash(this.password, salt)
+    this.password = hashedPassword
+    next()
+  }catch (error) {
+    next (error)
+  }
+})
+
+
+// User_detailsSchema.post('save', async function (next) {
+//   try{
+//     console.log("Called after saving a user")
+//   }catch (error) {
+//     next (error)
+//   }
+// })
 
 module.exports = mongoose.model('User_details', User_detailsSchema)
