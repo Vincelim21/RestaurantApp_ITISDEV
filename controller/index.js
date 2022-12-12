bd482a353a1d1df451f2754738822782d96e18ce
Kaanaaaaachan <94956335+InigoMiguelArada@users.noreply.github.com>
2022-12-12 17:08:25 +0800

    hashed password on database
    missing login and session

--- controller/index.js
+++ controller/index.js
@@ -3,17 +3,47 @@ const router = express.Router()
 const UserDetailsModel = require('../models/user_details')
 const mongoose = require('mongoose')
 const db = mongoose.connection
+const bcrypt = require('bcrypt')
 
 router.get('/', async(req,res) =>{
     res.render('login')
 })
 
 router.get('/register', async(req,res) =>{
-    res.render('register')
+    try{
+        const user_first = await UserDetailsModel.find({})
+        const params = {
+            user: user_first
+        } 
+        res.render('register',params) // Render EJS with table values
+    }catch(error){
+        res.status(500).send(error)
+        console.log(error)
+    }
 })
 
-router.get('/register', async(req,res) =>{
-    res.render('register')
+router.post('/register', async (req,res) =>{
+    try {
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        this.password = hashedPassword
+        const user = new UserDetailsModel({
+            userID: req.body.userid,
+            firstName: req.body.firstname,
+            lastName: req.body.firstname,
+            email: req.body.email,
+            password: hashedPassword,
+            userTypeName: req.body.usertype
+        })
+        console.log(this.email, this.password)
+        UserDetailsModel.create(user)
+        res.redirect('/')
+        }
+     catch (error){
+        console.log(error)
+        res.redirect('/register')
+    }
+    console.log(UserDetailsModel)
 })
 
 router.get('/home_stockctrl',(req,res) =>{
