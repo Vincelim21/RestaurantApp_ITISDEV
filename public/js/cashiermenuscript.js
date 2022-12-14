cdcadf7c3c3649c8fcc5a5c3bc4c2a0df956395d
GabCatacutan <gabriel_catacutan@dlsu.edu.ph>
2022-12-14 20:43:58 +0800

    Added input validation for cashier_menu
    Client side validation only

--- public/js/cashiermenuscript.js
+++ public/js/cashiermenuscript.js
@@ -1,11 +1,34 @@
 function addItem(id){
 
-    $(".table-header").after('<tr class="table-input">\
-    <td><input type="text" value="'+id+'" name="item_value" readonly></td>\
-    <td><input type="text" name="quantity_value"></td>\
-    <td>PRICE</td>\
-    </tr>')
-   
-    
+    let dupemessage;
+
+    if(document.getElementsByClassName("table-input").length == 0){
+        $("#beforecontinue").after('<button class="continue" type="submit" href="/">CONTINUE</button>')
+    }
+    let duplicate = false;
+    duplicate=false;
+    for(let i=0;i<document.getElementsByClassName("table-input").length && duplicate != true;i++){
+        // console.log(document.getElementsByClassName("tablevalue")[i].id)
+        console.log(id)
+        if(id == document.getElementsByClassName("tablevalue")[i].id){
+            duplicate = true;
+            dupemessage = id + " has already been addded"
+            $("#errormsg").html(dupemessage)
+        }
+        else{
+            duplicate = false;
+            
+        }
+    }
+    console.log("Duplicate is " + duplicate)
+    if(!duplicate){
+        $("#errormsg").empty()
+        $(".table-header").after('<tr class="table-input">\
+        <td><input class="tablevalue" "type="text" id ="'+id+'"value="'+id+'" name="item_value" readonly></td>\
+        <td><input type="number" name="quantity_value" required></td>\
+        <td>PRICE</td>\
+        </tr>')
+    }
+
 
 }
