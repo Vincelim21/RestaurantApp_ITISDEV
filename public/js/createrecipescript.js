f3d1427914e442eea86372429b15fa82f835f5e0
GabCatacutan <gabriel_catacutan@dlsu.edu.ph>
2022-12-14 17:15:40 +0800

    Update create_recipe

--- public/js/createrecipescript.js
+++ public/js/createrecipescript.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
     $("#add_ingredient").click(function(){
-
+        $(".ingredientcontainer").after('<div class="next"></div>')
         const name = $('#ingredient_name').clone();
         const value = $('#ingredient_value').clone();
         const unit = $('#ingredient_unit').clone();
         
-        $(name).appendTo(".ingredients_in_recipe")
-        $(value).appendTo(".ingredients_in_recipe")
-        $(unit).appendTo(".ingredients_in_recipe")
+        $(name).appendTo(".next")
+        $(value).appendTo(".next")
+        $(unit).appendTo(".next")
         
-
+        $(".next").removeAttr("class")
+        
         console.log("asd")
     })
     
